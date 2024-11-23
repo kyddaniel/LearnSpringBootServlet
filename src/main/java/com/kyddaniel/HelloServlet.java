eb1ed29612f5ebe2b55190364001516ad7e27449
kyddaniel <mksuenaa@connect.ust.hk>
2024-11-23 13:50:38 +0800

    Implement a response

--- src/main/java/com/kyddaniel/HelloServlet.java
+++ src/main/java/com/kyddaniel/HelloServlet.java
@@ -3,13 +3,25 @@ package com.kyddaniel;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 // Default URL is localhost:8080
 public class HelloServlet extends HttpServlet {
 
     // define a service() method for a servlet
-    public void service(HttpServletRequest request, HttpServletResponse response) {
+    // doGet() for a "GET" request
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         System.out.println("In service!");
+
+        PrintWriter writer = response.getWriter();
+
+        // response content (a HTML page)
+        //writer.println("Hello World");
+
+        // content can be in HTML
+        response.setContentType("text/html");
+        writer.println("<h1>Hello World</h1>");
     }
 
 }
